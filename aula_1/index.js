130800dc0171096c2824a59903cdd22df8150a18
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-05-29 08:44:02 -0300

    aula1-dio-typescript

---
+++ aula_1/index.js
@@ -0,0 +1,9 @@
+const soma = (a, b) => {
+    console.log(a + b);
+};
+
+soma (2, 5);
+soma ('Phillipe', 'Macedo'); //Se observar, o Javascript não tipifica a variável como no Typescript, por isso ele concatena as strings.
+// Para uma aplicação mais robusta, não é muito recomendado Js por esse motivo, tipificação de variaveis
+
+
